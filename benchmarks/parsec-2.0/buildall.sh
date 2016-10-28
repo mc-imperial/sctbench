c126d2741f4fa041ad06a2071f8424d54e62e4c1
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-10-28 09:43:02 +0100

    Just build the parsec benchmarks we need.

--- benchmarks/parsec-2.0/buildall.sh
+++ benchmarks/parsec-2.0/buildall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source env.sh
-parsecmgmt -a build -p apps kernels
+parsecmgmt -a build -p streamcluster streamcluster2 streamcluster3 ferret
 #parsecmgmt -a build -p openmp -c gcc-openmp
 
