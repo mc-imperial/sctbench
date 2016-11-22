41ad8832b3107322ae2ec6ab4dc08d8324c7eb54
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-11-22 19:19:27 +0000

    Do an initial dummy run of each parsec benchmark to create run directories.

--- benchmarks/parsec-2.0/buildall.sh
+++ benchmarks/parsec-2.0/buildall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source env.sh
 parsecmgmt -a build -p streamcluster streamcluster2 streamcluster3 ferret
+parsecmgmt -a run -p streamcluster streamcluster2 streamcluster3 ferret -s echo -n 2 -i test
 #parsecmgmt -a build -p openmp -c gcc-openmp
 
